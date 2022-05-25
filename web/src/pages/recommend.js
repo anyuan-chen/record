cc23010d3dc4615e2f46fbfaf5b79c36c35cf1f2
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-05-25 15:34:01 -0400

    temp recommend

--- web/src/pages/recommend.js
+++ web/src/pages/recommend.js
@@ -1,6 +1,15 @@
 import BaseLayout from "../components/layouts/baselayout";
 import Song from "../components/recommend/song";
 import "./recommend.css";
+import { motion } from "framer-motion";
+const ButtonVariants = {
+  unhovered: {},
+  hovered: {
+    backgroundColor: "black",
+    color: "white",
+  },
+};
+
 const Recommend = () => {
   return (
     <BaseLayout>
@@ -16,8 +25,45 @@ const Recommend = () => {
           <h3 className="h4 light">
             here are some songs i think you'd enjoy...
           </h3>
-          <Song></Song>
+          <Song title="hi" artist="jay chou" album="common jasmine"></Song>
+          <Song title="hi" artist="jay chou" album="common jasmine"></Song>
+          <Song title="hi" artist="jay chou" album="common jasmine"></Song>
         </div>
+        <div className="recommendation__cta">
+          <h1 className="h0">i have more for you.</h1>
+          <h2 className="h2 fade">
+            if you want, I can add a playlist on spotify for you
+          </h2>
+          <motion.a href="/dashboard" style={{ textDecoration: "none" }}>
+            <motion.button
+              initial="unhovered"
+              whileHover="hovered"
+              animate="unhovered"
+              variants={ButtonVariants}
+              className="lander__button"
+            >
+              <p className="h4">yes please!</p>
+            </motion.button>
+          </motion.a>
+        </div>
+        <div className="recommendation__cta">
+          <h1 className="h1 light"> that’s all i have for you right now....</h1>
+          <h2 className="h2 fade light">
+            make sure to check back soon! i might have something new ;)
+          </h2>
+          <motion.a href="/dashboard" style={{ textDecoration: "none" }}>
+            <motion.button
+              initial="unhovered"
+              whileHover="hovered"
+              animate="unhovered"
+              variants={ButtonVariants}
+              className="lander__button"
+            >
+              <p className="h4">yes please!</p>
+            </motion.button>
+          </motion.a>
+        </div>
+        <div style={{ marginBottom: "8rem" }}></div>
       </div>
     </BaseLayout>
   );
