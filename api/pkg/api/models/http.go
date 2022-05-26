15d299a19ffb59773f6ac9bc7aa5074b9bfd094d
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-05-26 19:07:59 -0400

    api structure

---
+++ api/pkg/api/models/http.go
@@ -0,0 +1,19 @@
+package models
+
+import "net/http"
+
+type HttpService interface {
+	TopArtists(w http.ResponseWriter, r *http.Request)
+	TopSongs(w http.ResponseWriter, r *http.Request)
+	TopGenres(w http.ResponseWriter, r *http.Request)
+	
+	SongCollage(w http.ResponseWriter, r *http.Request)
+	ArtistCollage(w http.ResponseWriter, r *http.Request)
+
+	AveragePopularity(w http.ResponseWriter, r *http.Request)
+	RecommendedSongs(w http.ResponseWriter, r *http.Request)
+	MakePlaylist(w http.ResponseWriter, r *http.Request)
+	
+	SpotifyLogin(w http.ResponseWriter, r *http.Request)
+	SpotifyCallback(w http.ResponseWriter, r *http.Request)
+}
