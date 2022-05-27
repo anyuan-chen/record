3571649b1164eb4db1cb33eda5b0a1c0143f8cd1
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-05-27 11:20:43 -0400

    login endpoint api

--- api/pkg/api/http/api.go
+++ api/pkg/api/http/api.go
@@ -1,2 +1,22 @@
 package http
 
+import "github.com/anyuan-chen/record/api/pkg/api/models"
+
+type HttpService struct {
+	httpService models.HttpService
+}
+// type HttpService interface {
+// 	TopArtists(w http.ResponseWriter, r *http.Request)
+// 	TopSongs(w http.ResponseWriter, r *http.Request)
+// 	TopGenres(w http.ResponseWriter, r *http.Request)
+
+// 	SongCollage(w http.ResponseWriter, r *http.Request)
+// 	ArtistCollage(w http.ResponseWriter, r *http.Request)
+
+// 	AveragePopularity(w http.ResponseWriter, r *http.Request)
+// 	RecommendedSongs(w http.ResponseWriter, r *http.Request)
+// 	MakePlaylist(w http.ResponseWriter, r *http.Request)
+	
+// 	SpotifyLogin(w http.ResponseWriter, r *http.Request)   login route
+// 	SpotifyCallback(w http.ResponseWriter, r *http.Request)  login route
+// }
---
+++ api/pkg/api/http/login.go
@@ -0,0 +1,47 @@
+package http
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+)
+
+var auth = spotifyauth.New(spotifyauth.WithRedirectURL(os.Getenv("redirectURL")), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+
+
+func (s *HttpService) SpotifyLogin(w http.ResponseWriter, r *http.Request){
+	expiration := time.Now().Add(20 * time.Minute)
+	state := make(map[string]interface{})
+	randomizedState := make([]byte, 16)
+	rand.Read(randomizedState)
+	state["random"] = randomizedState;
+	json, err := json.Marshal(state);
+	if err != nil {
+		http.Error(w, "A JSON Encoding error has been encountered.", http.StatusInternalServerError)
+	}
+	encoded_json := base64.URLEncoding.EncodeToString(json)
+	url := auth.AuthURL(encoded_json)
+	cookie := http.Cookie{Name: "oauthstate", Value: encoded_json, Expires: expiration}
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func (s *HttpService) SpotifyCallback(w http.ResponseWriter, r *http.Request){
+	state, err := r.Cookie("oauthstate")
+	if err != nil || r.FormValue("state") != state.Value {
+		http.Error(w, "Bad OAuth State", http.StatusInternalServerError)
+	}
+	token, err := auth.Token(r.Context(), state.Value, r)
+	if err != nil {
+		http.Error(w, "Failed to Retrieve Token", http.StatusInternalServerError)
+	}
+	//need to save this token somewhere 
+	
+	//client := spotify.New(auth.Client(r.Context(), token))
+}
