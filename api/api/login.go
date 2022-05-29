e70082e79938c66e6fb30eff7e21ffafa992eb0f
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-05-29 14:15:21 -0400

    grpc tests

--- api/api/login.go
+++ api/api/login.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
@@ -9,7 +10,9 @@ import (
 	"os"
 	"time"
 
+	"github.com/anyuan-chen/record/proto/pkg/session_manager_pb"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2"
 )
 
 var auth = spotifyauth.New(spotifyauth.WithRedirectURL(os.Getenv("REDIRECT_URL")), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
@@ -41,8 +44,22 @@ func (s *HttpService) SpotifyCallback(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		http.Error(w, "Failed to Retrieve Token", http.StatusInternalServerError)
 	}
-	fmt.Print(token)
+	
 	//need to save this token somewhere  and process it (probably like grpc save token)
 	//client := spotify.New(auth.Client(r.Context(), token))
+	token_json, err := json.Marshal(token)
+	var token_two oauth2.Token
+	json.Unmarshal(token_json, &token_two)
+	fmt.Print(token == &token_two)
+	if err != nil {
+		http.Error(w, "Error encoding JSON token", http.StatusInternalServerError)
+	}
+	session_id, err := s.Session_manager.CreateSession(context.Background(), &session_manager_pb.Token{Token: token_json})
+
+	if err != nil {
+		http.Error(w, "Problem with the session management service: " + err.Error(), http.StatusInternalServerError)
+	}
+	cookie := http.Cookie{Name: "session_id", Value: session_id.Code}
+	http.SetCookie(w, &cookie)
 	http.Redirect(w, r, os.Getenv("FRONTEND_URL") + "/dashboard", http.StatusPermanentRedirect)
 }
