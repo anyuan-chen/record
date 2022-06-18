4a8687896a1d4724397d7f10c79ef5c635ea9135
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-06-17 20:11:43 -0400

    Revert "Delete web_archive directory"
    This reverts commit f9d2182c0433b905e294ebb1a8223ecb6b1c765f.

---
+++ web_archive/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './globals/index.css';
+import './globals/textstyles.css'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
---
+++ web_archive/src/pages/dashboard.js
@@ -0,0 +1,44 @@
+import BaseLayout from "../components/layouts/baselayout";
+import { motion } from "framer-motion";
+import DashboardElement from "../components/dashboard/dashboardelement";
+import "./dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <motion.div
+      style={{ height: "100vh" }}
+      initial={{ opacity: 0.3 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <BaseLayout>
+        <h1 className="h1 bold"> hi, andrew</h1>
+        <div className="dashboard__container">
+          <DashboardElement
+            src="https://wallpaperaccess.com/full/3547007.jpg"
+            alt="your music photos"
+            label="about you"
+            caption="either going to boost or destroy your ego"
+            href="/aboutyou"
+          ></DashboardElement>
+          <DashboardElement
+            src="https://wallpaperaccess.com/full/3547007.jpg"
+            alt="your music photos"
+            label="recommendations"
+            caption="do you really trust an AI with your music?"
+            href="/recommend"
+          ></DashboardElement>
+          <DashboardElement
+            src="https://wallpaperaccess.com/full/3547007.jpg"
+            alt="your music photos"
+            label="log off"
+            caption="see you soon!"
+            href="/"
+          ></DashboardElement>
+        </div>
+      </BaseLayout>
+    </motion.div>
+  );
+};
+export default Dashboard;
